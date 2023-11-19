6789f6a3243604aaf25083f14722cdd686568628
Edgar Roberto Martinez Mendoza <edgar.martinezm@elektra.com.mx>
2023-11-19 17:33:25 -0600

    error sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MapComponent } from './components/map/map.component';
+
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapLibreGLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
